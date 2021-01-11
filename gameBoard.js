5dfef444d4d61338bf3f1014606f63e3bfdabb51
Olga Kolosova <panther28kob@gmail.com>
2021-01-11 16:44:47 +0300

    Release:0-1 Done

--- gameBoard.js
+++ gameBoard.js
@@ -1,16 +1,32 @@
 module.exports = class GameBoard {
-    constructor(terrainHelper, terrainList, size) {
-        this.terrainHelper = terrainHelper;
-        this.terrainList = terrainList;
-        this.size = size;
-    }
+  constructor(terrainHelper, terrainList, size) {
+    this.terrainHelper = terrainHelper;
+    this.terrainList = terrainList;
+    this.size = size;
+  }
 
-    terrainMap() {
-        // напишите этот метод сами
-    }
+  terrainMap() {
+    let elArr = [];
+    let resArr = [];
+    this.terrainList.forEach((el, idx) => {
 
-    difficultyOfJourney() {
-        // напишите этод метод сами
-    }
+      const word = this.terrainHelper.letterToTerrain(el);
+      if (typeof word == 'string')
+        elArr.push(word);
+      if (idx !== 0 && (idx + 1) % this.size === 0) {
+        resArr.push(elArr);
+        elArr = [];
+      }
+    });
+    return resArr;
+    // напишите этот метод сами
+  }
+
+  difficultyOfJourney(arr) {
+    const mapArr = this.terrainMap();
+    const res = arr.reduce((sum, item) => sum + this.terrainHelper.scoreDifficulty(mapArr[item[0]][item[1]]), 0);
+    return res
+    // напишите этод метод сами
+  }
 }
 
